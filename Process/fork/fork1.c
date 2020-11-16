e1431e97efbda0cee8b11e568300c60dae748a3c
dragon-web <1582943265@qq.com>
2020-11-16 22:11:44 +0800

    Process/

---
+++ Process/fork/fork1.c
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<unistd.h>
+
+int main()
+{
+  printf("begin-----------------\n");
+  pid_t ret = fork();
+  if(ret < 0)
+  {
+    perror("fork");
+    return 0;
+  }
+  else if(ret == 0)
+  {
+    {
+      printf("I am child,pid = %d,ppid = %d\n",getpid(),getppid());
+      sleep(1);
+    }
+  }
+  else 
+  {
+    while(1)
+    {
+      printf("I am father,pid = %d,ppid = %d\n",getpid(),getppid());
+      sleep(1);
+
+    }}
+
+
+  return 0;
+}
