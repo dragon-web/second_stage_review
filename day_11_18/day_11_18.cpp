1cf0a4afb1934a1ef72d70cbe193584fd970f3e8
dragon-web <1582943265@qq.com>
2020-11-18 16:40:11 +0800

    day_11_18/

---
+++ day_11_18/day_11_18.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+class Solution {
+  public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+      if (t1 == nullptr) {
+        return t2;
+
+      }
+      if (t2 == nullptr) {
+        return t1;
+
+      }
+      TreeNode* merged = new TreeNode(t1->val + t2->val);
+      merged->left = mergeTrees(t1->left, t2->left);
+      merged->right = mergeTrees(t1->right, t2->right);
+      return merged;
+
+    }
+
+};
+
+int main()
+{
+
+}
