b1022dfa9fdebd25b64fe75a86d760b074eccb69
dragon-web <1582943265@qq.com>
2020-12-03 18:35:37 +0800

    Process_Communication/

---
+++ Process_Communication/named_pipe/test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main()
+{
+  int fd = open("./fifo",O_RDWR);
+  if(fd < 0)
+  {
+    perror("open");
+    return -1;
+  }
+  write(fd,"i love you",10);
+  printf("write down\n");
+
+
+  while(1)
+  {
+    sleep(1);
+  }
+close(fd);
+  return 0;
+}
---
+++ Process_Communication/named_pipe/test1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main()
+{
+  int fd = open("./fifo",O_RDWR);
+  if(fd < 0)
+  {
+    perror("open");
+    return -1;
+  }
+  char buf[1024] = {0};
+  read(fd,buf,sizeof(buf) - 1);
+  printf("buf = %s",buf);
+  while(1)
+  {
+    sleep(1);
+  }
+  close(fd);
+  return 0;
+}
