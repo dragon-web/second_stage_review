ad228f7c107e317b8365f95b2cae6ebecb1e18fe
dragon-web <1582943265@qq.com>
2021-03-02 12:16:27 +0800

    string

--- STL/StringTotal.cpp
+++ STL/StringTotal.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 class String
 {
+    public :
+        typedef char* iterator;
     public:
     String(const char* str = "")
     {
@@ -12,16 +14,16 @@ class String
         {
             str = "";
         }
-        capacity = strlen(str);
-        size = strlen(str);
+        _capacity = strlen(str);
+        _size = strlen(str);
         _str = new char[strlen(str) + 1];
         strcpy(_str,str);
     }
 
-    String (const String & s) : size(0),capacity(0),_str(nullptr)
+    String (const String & s) : _size(0),_capacity(0),_str(nullptr)
     {
-       size = s.size;
-       capacity = s.capacity;
+       _size = s._size;
+       _capacity = s._capacity;
        _str = new char[strlen(s._str) + 1];
        strcpy(_str,s._str);
     }
@@ -30,19 +32,70 @@ class String
         if(&s != this)
         {
             _str = new char[strlen(s._str) + 1];
-            capacity = s.capacity;
-            size = s.size;
-            capacity = s.capacity;
+            _capacity = s._capacity;
+            _size = s._size;
+            _capacity = s._capacity;
             strcpy(_str,s._str);
         }
         return *this;
     }
+    ~String()
+    {
+        if(_str)
+        {
+            delete []_str;
+            _capacity = 0;
+            _size = 0;
+            _str = nullptr;
+        }   
+    }   
+    iterator begin()
+    {
+        return _str;
+    }
+    iterator end()
+    {
+        return _str + _size;
+    }
+    size_t size() const 
+    {
+        return _size;
+    }
+    size_t capacity() const 
+    {
+        return _capacity;
+    }
+    bool empty() const 
+    {
+        return _size == 0;
+    }
+    void clear()
+    {
+        _str[0] = '\0';
+        _size = 0;
+    }
+    void reserve(size_t newcapacity)
+    {
+        if(newcapacity > capacity())
+        {
+            char* temp = new char[newcapacity + 1];
+            strcpy(temp,_str);
+            delete[] _str;
+            _str = temp;
+            _capacity = newcapacity;
+        }
+    }
+    void resize(size_t newsize , char ch)
+    {
+        while(newsize > _capacity)
+        {
+            
+        }   
 
-
-
+    }
     private:
-        size_t size;
-        size_t capacity;
+        size_t _size;
+        size_t _capacity;
         char* _str;
         static size_t npos;
 };
