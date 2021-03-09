40b8cc7e670d3ffb3b836bf372e9399ff53abd89
dragon-web <1582943265@qq.com>
2021-03-09 22:31:54 +0800

    STL

--- STL/List.cpp
+++ STL/List.cpp
@@ -20,5 +20,48 @@ template<class T>
 
 struct ListIterator
 {
+    typedef ListNode<T> Node;
+    typedef ListIterator<T> Self;
+    
+    ListIterator(Node* str = nullptr) : _ptr(str)
+    {}
+    Self& operator* ()
+    {
+        return _ptr->data; 
+    }
+    Self& operator->() 
+    {
+        return  &_ptr->data;
+    }
+    Self& operator++()
+    {
+        _ptr = _ptr->next;
+        return (*this);
+    }
+    Self& operator++(int)
+    {
+       Self tmp(*this);
+       _ptr = _ptr->next;
+       return tmp;
+    }
+    Self& operator-- ()
+    {
+        _ptr = _ptr->prev;
+        return (*this);
+    }
+    Self& operator--(int)
+    {
+        Self tmp(*this);
+        _ptr = _ptr->prev;
+        return tmp;
+    }
+    bool operator != (const Self& L) const
+    {
+        return _ptr != L._ptr;
+    }
+    bool operator == (const Self L) const{
+        return _ptr != L._ptr;
+    }
 
+     Node* _ptr;
 };
